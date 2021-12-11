b3b77fddeeebfd66ff34075f7b62e50a2396353a
Andrei Gheorghita <gheorghita.andrei001@gmail.com>
2021-12-11 15:46:28 +0200

    Code cleanup & added GetAuthors method

--- OnLib/OnLib.Server/BooksManager.h
+++ OnLib/OnLib.Server/BooksManager.h
@@ -13,12 +13,11 @@ class BooksManager
 {
 private:
 	sqlite::database& database;
-	void GetAuthorsAndOtherCategories(const std::vector<data::Book>& books);
-
 public:
 	BooksManager(sqlite::database& db) :database(db) {};
 	std::vector<data::Book> GetNewestFiveBooksFromEachCategory();
 	std::vector<data::LendBook>GetLendedBooks(uint64_t bookId);
 	void AddLendedBookToUser(uint64_t bookId, uint64_t userId);
+	void GetAuthors(data::Book& book);
 };
 
